48e1b1bc32f490140303d2d7bb0181d4c5507241
lauraol <lauraoliveirape@gmail.com>
2022-08-18 22:53:31 -0300

    adicionando procfile, configuração para deploy utilizando o heroku

--- src/database/database.js
+++ src/database/database.js
@@ -2,10 +2,13 @@ const mongoose = require('mongoose');
 
 const connectToDatabase = () => {
   mongoose
-    .connect('mongodb://localhost:27017/guide-dev-teacher-db', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(
+      'mongodb+srv://Laurou:Banqu1nhodeDados@cluster0.co1nezj.mongodb.net/?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      },
+    )
     .then(() => console.log('MongoDB conectado! ;)'))
     .catch((error) => {
       console.log(
