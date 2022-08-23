c4bf52055c373a64f4fe6767c89bedcda86b3aa0
lauraol <lauraoliveirape@gmail.com>
2022-08-23 17:27:16 -0300

    alterações para poder fazer o deploy no heroku, agora vai

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const routes = require('./src/routes/termo.route');
 const connectToDatabase = require('./src/database/database');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 connectToDatabase();
