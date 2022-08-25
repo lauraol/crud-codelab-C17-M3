89050497290cf56f85e9041314c4b9c8cacfd127
lauraol <lauraoliveirape@gmail.com>
2022-08-25 19:49:15 -0300

    adicionando arquivo de rota do login

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const { config } = require('dotenv');
 const routes = require('./src/routes/termo.route');
 const routesUsers = require('./src/routes/usuario.route');
-//const routeLogin = require('./routes/auth.routes');
+const routeLogin = require('./routes/auth.routes');
 const connectToDatabase = require('./src/database/database');
 
 const port = process.env.PORT || 3000;
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/termos', routes);
 app.use('/usuarios', routesUsers);
-//app.use('/login', routeLogin);
+app.use('/login', routeLogin);
 app.use('/api-docs', routes);
 
 app.listen(port, () => {
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const authController = require('../controllers/auth.controller');
+
+router.post('/login', authController.loginController);
+
+module.exports = router;
