71aefd07f4d597c4d513f7af864cbc487c272a4f
lauraol <lauraoliveirape@gmail.com>
2022-08-25 19:57:33 -0300

    demonstração de pull request

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+//const lalala;
 
 router.post('/login', authController.loginController);
 
