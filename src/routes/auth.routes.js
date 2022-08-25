e7cd25db94d6cf5c1821a1fb3cb7137d9dd719de
Laura Oliveira Pedro <57760301+lauraol@users.noreply.github.com>
2022-08-25 20:00:30 -0300

    Revert "adicionando arquivo de rota do login"

--- src/routes/auth.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const authController = require('../controllers/auth.controller');
-
-router.post('/login', authController.loginController);
-
-module.exports = router;
