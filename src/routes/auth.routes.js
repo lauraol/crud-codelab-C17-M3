2bd103ee9533cda1b498d6d7839cfad7b36fed48
lauraol <lauraoliveirape@gmail.com>
2022-08-25 19:59:13 -0300

    removendo const lalala

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
-//const lalala;
 
 router.post('/login', authController.loginController);
 
